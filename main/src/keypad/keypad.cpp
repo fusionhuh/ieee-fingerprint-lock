690806eae56953f3e1648c06d4d9aba69714e3ed
Josh <joshk559@gmail.com>
2023-03-24 01:20:20 -0500

    Fix incorrect code for Adafruit keypad

--- main/src/keypad/keypad.cpp
+++ main/src/keypad/keypad.cpp
@@ -1,13 +1,35 @@
 #include <heltec.h>
-#include "keypad.hpp"
+#include <Adafruit_Keypad.h>
+
+#define KEYPAD_PID1332
+
+#define R1 GPIO_NUM_2
+#define C2 GPIO_NUM_3
+#define C1 GPIO_NUM_4
+#define C4 GPIO_NUM_5
+#define C3 GPIO_NUM_6
+
+#include "keypad_config.h"
+
+static Adafruit_Keypad custom_keypad = Adafruit_Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 void keypad_setup() {
-    pinMode(BUTTON_1, INPUT);
-    pinMode(BUTTON_2, INPUT);
-    pinMode(BUTTON_3, INPUT);
-    pinMode(BUTTON_4, INPUT);
+    custom_keypad.begin();
 }
 
-bool is_button_pressed(uint32_t button) {
-    return digitalRead(button) == HIGH;
+bool is_button_pressed(char button) {
+    custom_keypad.tick();
+    if (custom_keypad.available()) {
+        keypadEvent event = custom_keypad.read();
+        if (event.bit.EVENT == KEY_JUST_PRESSED) {
+            return ((char)event.bit.KEY == button);
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        Serial.println("Keypad is not available.");
+        return false;
+    }
 }
--- main/src/keypad/keypad.hpp
+++ main/src/keypad/keypad.hpp
@@ -1,14 +1,10 @@
-// correspond to buttons on keypad
-// for now, say GPIO pins 1-4 map to buttons 1-4
-enum buttons { 
-  NO_BUTTON = -1,
-  BUTTON_1 = GPIO_NUM_1,
-  BUTTON_2 = GPIO_NUM_2,
-  BUTTON_3 = GPIO_NUM_3,
-  BUTTON_4 = GPIO_NUM_4
+enum buttons {
+  BUTTON_1 = '1',
+  BUTTON_2 = '2',
+  BUTTON_3 = '3',
+  BUTTON_4 = '4'
 };
-
 
 void keypad_setup();
 
-bool is_button_pressed(uint32_t button);
+bool is_button_pressed(char button);
