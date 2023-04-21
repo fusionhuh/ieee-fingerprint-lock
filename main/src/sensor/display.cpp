c3064d52e02405d5979125d5e9eb2032a147947d
step-cheng <stephenscheng111@gmail.com>
2023-04-21 18:20:53 -0500

    Display Code

---
+++ main/src/sensor/display.cpp
@@ -0,0 +1,25 @@
+#include "display.hpp"
+
+void display_setup()
+{
+	Serial.begin(115200);
+	if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+	{
+		Serial.println(F("SSD1306 allocation failed"));
+	}
+}
+
+void display_string(string message)
+{
+	display.clearDisplay();
+	display.setTextSize(1);
+	display.setTextColor(WHITE);
+	display.setCursor(0, 10);
+	display.println(message);
+	display.display();
+}
+
+void display_clear()
+{
+	display.clearDisplay();
+}
---
+++ main/src/sensor/display.hpp
@@ -0,0 +1,14 @@
+#include <Adafruit_Fingerprint.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void display_setup();
+
+void display_string(string message);
+
+void display_clear();
